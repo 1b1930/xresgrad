1f61cb9d3b1bae14c9046d9cfa416099c2f83eae
aaaaaaaa <aaaaaaaa>
2020-04-29 19:55:35 -0300

    first working state

--- main.py
+++ main.py
@@ -1,105 +1,53 @@
-import sys
+import sys, os, fileinput
 
+# string that will be deleted from line extracted from xresources
+# necessary to extract correct hex value
 fgdel = "*.foreground:"
 bgdel = "*.background:"
+
+# xresources path, will change for release
 xpath = ".Xresources"
 
-# throw error if no arguments
-# maybe default to bg instead of exiting the program?
-if len(sys.argv) <= 1:
-    print("ERROR: Specify which color you want to use as base for the gradients!")
+# maps argument variables to cmd arguments
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+arg3 = sys.argv[3]
+
+# turns offset argument into an int (from str)
+# necessary for offset range error checking
+arg2 = int(arg2)
+
+# this can possibly go, i don't know, i'll think about it, maybe
+valid_args = [ "-bg", "-fg" ]
+
+# Self-explanatory, basic error checking
+if arg1 not in valid_args:
+    print("ERROR: Invalid color argument")
+    exit(1)
+elif arg2 < 0 or arg2 > 30000:
+    print("ERROR: Invalid offset range")
+    exit(1)
+elif arg3 not in ["add", "sub"]:
+    print("ERROR: Invalid operator, valid operators: \"add\" and \"sub\"")
     exit(1)
 
-# simple argument test
-arg1 = sys.argv[1]
-
-# arg2 = sys.argv[2]
-
-# DEBUG #
-# prints all arguments and numbers em
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i], i)
-
-# count the number of arguments
-# len(sys.argv)
-# shouldn't let argument number surpass how many args i want
-
-#----------#
-
-# For simplicity, i'll only accept one argument and two possibilities for now
-valid_args = ["-bg", "-fg"]
-
-# check if arguments are valid, mostly debug but will be used for error handling
-if str(arg1) == valid_args[0] or valid_args[1]:
-    print("works")
-else:
-    print("\nHELP: -bg for background, -fg for foreground")
-
+# Don't know if this needs to be here, but it gives me an error if i delete it so fuck it
 grad_color = "gibberish"
 
+# stores either bgdel or fgdel into it's actual variable to be used by functions
 if str(arg1) == valid_args[0]:
     grad_color = bgdel
-    print(grad_color)
 elif str(arg1) == valid_args[1]:
     grad_color = fgdel
-    print(grad_color)
-else:
-    print("invalid stuff etc")
 
-#----------#
-
-# calc function outline
-# how do i even start manipulating hex?
-# add '0x' to the start of the hex color value, then do math as normal, using a int as operand
-# it will return a int value, which can then be converted to hex with the hex() function
-# ...simple?
-
-#----------#
-
-# open file and search for string function
-# this might be the hardest one
-
-# this function doesn't need to be here, it's too complicated, i'll simplify it later
-fpath = "testfile"
-def openf(f_path, access_mode):
-    if access_mode == "r":
-        file1 = open(fpath, "r")
-
-    elif access_mode == "rw":
-        file1 = open(fpath, "r+")
-
-    elif access_mode == "ar":
-        file = open(fpath, "a+")
-
-    else:
-        print("invalid access mode, fix it")
-        exit(1)
-
-# refactor this mountain of garbage please
-
-#file1 = open('.Xresources')
-
-#bgdel = "*.background:"
-#fgdel = "*.foreground:"
-#for line in file1:
-#    if fgdel in line:
-#        line2 = line.replace("#", '0x')
-#        line2 = line2.replace(" ", '')
-#        line2 = line2.replace("  ", '')
-#        line2 = line2.replace(fgdel, '')
-#        print(line2)
-#        line2 = line2.replace('\n', '')
-#        # can't turn hex into int, have to calculate shit before
-#        line3 = int(line2, 16) + 500
-#        line3 = hex(line3)
-#        print(line3)
 #
-#        print("it finds it")
-#file1.close()
+#
+#   function definitions
+#
+#
 
 # get_hex: takes a file path and a line identifier (depending on which color you want)
 
-# testing get_hex
 def get_hex(fpath, color):
     
     file1 = open(fpath)
@@ -109,39 +57,81 @@ def get_hex(fpath, color):
             line2 = line2.replace("#", '')
             line2 = line2.replace(" ", '')
             line2 = line2.replace("\n", '')
-            print(line2)
             file1.close()
             return(line2)
-        
-        # this doesn't fucking work for some reason, i have no idea
-#        else:
-#            return("fuck")
-#            print("error: couldn't find xresources color in file specified")
-#            file1.close()
-#            exit(1)
 
-def do_gradient(color, offset, opr):
-    if color == "" or offset <= 0:
-        print("error: invalid color or invalid offset")
-        exit(1)
-    elif opr == "add":
-        i = int(color, 16)
+
+
+# takes a color (in hex!), it's offset (how much to add or subtract from it) and a operator (str: "add" or "sub")
+def do_gradient(hexcolor, offset, opr):
+    if opr == "add":
+        i = int(hexcolor, 16)
         i = i + offset
         hexi = hex(i)
         return(hexi)
+    
     elif opr == "sub":
-        i = int(color, 16)
+        i = int(hexcolor, 16)
         i = i - offset
         hexi = hex(i)
         return(hexi)
 
 
 
+# very simple function that appends a line to the end of the file specified
+def append_to_file(line, fname):
+    with open(fname, "a") as file1:
+        file1.write(line)
+        file1.write("\n")
+    return(0)
 
-grad = get_hex(xpath, grad_color)
-print(grad)
-test = do_gradient("d2d2d2", 3000, "sub")
-print(test)
 
-        
+def delete_multiple_lines(original_file, line_numbers):
+    """In a file, delete the lines at line number in given list"""
+    is_skipped = False
+    counter = 0
+    # Create name of dummy / temporary file
+    dummy_file = original_file + '.bak'
+    # Open original file in read only mode and dummy file in write mode
+    with open(original_file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        # Line by line copy data from original file to dummy file
+        for line in read_obj:
+            # If current line number exist in list then skip copying that line
+            if counter not in line_numbers:
+                write_obj.write(line)
+            else:
+                is_skipped = True
+                counter += 1
+    # If any line is skipped then rename dummy file as original file
+    if is_skipped:
+        os.remove(original_file)
+        os.rename(dummy_file, original_file)
+    else:
+        os.remove(dummy_file)
+
+
+## main function, kindof
+
+# should put this shit inside a function
+counter = 0
+for line in fileinput.input(xpath, inplace=True):
+    if not counter:
+        if line.startswith('! GRADIENTS') or line.startswith('*.grad'):
+            counter = 4;
+        else:
+            print(line, end='')
+    else:
+        counter -= 1
+
+with open(xpath, "a") as file1:
+    file1.write("\n! GRADIENTS\n")
+
+for i in range(0,10):
+    hexc = get_hex(xpath, grad_color)
+    hexc2 = do_gradient(hexc, arg2 * (i + 1), arg3)
+    hexc2 = hexc2.replace("0x", "#")
+    hexc_line = "*.grad" + str(i) + ":\t" + hexc2
+    print(hexc_line)
+    append_to_file(hexc_line, xpath)
+    ++i
 
