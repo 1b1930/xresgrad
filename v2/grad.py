223371770bdedd4b3bdc48d09a2ce83ec88ebadf
aaaaaaaa <aaaaaaaa>
2020-05-10 19:34:36 -0300

    v2: rewrite verify function

--- v2/grad.py
+++ v2/grad.py
@@ -16,11 +16,20 @@ def verify(xpath, desired_color):
 
 
 
-for i in range(0, len(valid_colors)):
-    if verify(xpath, valid_colors[i]) == True:
-        print("found index " + str(i))
-    else:
-        print("didn't find index " + str(i))
-    ++i
+#for i in range(0, len(valid_colors)):
+#    if verify(xpath, valid_colors[i]) == True:
+#        print("found index " + str(i))
+#    else:
+#        print("didn't find index " + str(i))
+#    ++i
     
+def verify2(xpath, color_list):
+    if isinstance(color_list, list):
+        with open(xpath, 'r') as file:
+            for line in file:
+                for i in range(0, len(color_list)):
+                    if color_list[i] in line:
+                        print("found " + color_list[i])
+                    ++i
 
+verify2(xpath, valid_colors)
